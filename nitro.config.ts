025a632e915d2d9f8ead32e53d38fdb79ecb434e
Misaina23 <andrianisaina23@gmail.com>
2026-08-04 03:00:23 +0300

    le cinquieme commit

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,7 +1,13 @@
+import { fileURLToPath } from "node:url";
 import { defineNitroConfig } from "nitro/config";
+
+const tslibEntry = fileURLToPath(new URL("./node_modules/tslib/tslib.js", import.meta.url));
 
 export default defineNitroConfig({
   preset: "vercel",
+  alias: {
+    tslib: tslibEntry,
+  },
   // Nitro externalizes some transitive deps (e.g. `tslib`) and expects them to be
   // present in the deployed function's node_modules. On Vercel they are not always
   // traced/copied, which crashes every request with ERR_MODULE_NOT_FOUND at runtime.
